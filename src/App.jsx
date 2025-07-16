431e881e2c125c9dd5a630c779ef3e48217d7350
theo-code1 <codingbusiness99@gmail.com>
2025-07-16 12:56:13 +0100

    Setup the assets and foonts...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <section className='h-[80dvh] flex items-center justify-center flex-col gap-16'>
+    <h1 className='text-4xl font-clash-display font-bold text-center'>Right Now! I'm going to build my Portfolio</h1>
+    <h1 className='text-4xl font-switzer font-bold text-center'>Right Now! I'm going to build my Portfolio</h1>
+    </section>
+  )
+}
+
+export default App
