e306b72bc38c903d4fc7c8b8cad83f209df480a4
theo-code1 <codingbusiness99@gmail.com>
2025-07-17 17:26:49 +0100

    Refactor code structure for improved readability and maintainability

---
+++ src/assets/Icons/CheckCircle.jsx
@@ -0,0 +1,20 @@
+function FiCheckCircle(props) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+      <polyline points="22 4 12 14.01 9 11.01" />
+    </svg>
+  );
+}
+
+export default FiCheckCircle;
