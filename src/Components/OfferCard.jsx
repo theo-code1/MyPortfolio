9e6edbe6a5929240593f4028ec2d1c3fe0f32a8d
theo-code1 <codingbusiness99@gmail.com>
2025-07-17 23:26:03 +0100

    Fix layout issues in OfferCard component; adjust spacing and improve skill tags styling

--- src/Components/OfferCard.jsx
+++ src/Components/OfferCard.jsx
@@ -5,17 +5,17 @@ const OfferCard = ({ IconChild, OfferTitle, OfferDesc, skillsBackground, skillsC
 
 
   return (
-    <article className='relative font-switzer w-[450px] h-[ px-8 pt-16 pb-6 rounded-xl bg-white  flex flex-col gap-8 items-center justify-between border border-black/20 shadow-[0_2px_24px_0_var(--color-shadow-black)] hover:shadow-[0_4px_32px_0_var(--color-shadow-black)] transition-all duration-200 cursor-pointer'>
+    <article className='relative font-switzer w-[450px] h-[ px-8 pt-16 pb-6 rounded-xl bg-white  flex flex-col gap- items-center justify-between border border-black/20 shadow-[0_2px_24px_0_var(--color-shadow-black)] hover:shadow-[0_4px_32px_0_var(--color-shadow-black)] transition-all duration-200 cursor-pointer'>
         <div className="offer-icon absolute -top-8 left-1/2 -translate-x-1/2 w-20 "
         >
             {IconChild}
         </div>
-        <h1 className='text-[28px] font-semibold text-black'>{OfferTitle}</h1>
-        <p className='text-lg text-black/75 mb-8'>{OfferDesc}</p>
-        <div className="skills flex flex-wrap gap-2 ">
+        <h1 className='text-[28px] font-semibold text-black mb-8'>{OfferTitle}</h1>
+        <p className='text-lg text-black/75 mb-16'>{OfferDesc}</p>
+        <div className="skills flex flex-wrap self-end gap-2 ">
           {skills.map((skill, index) => (
 
-            <span key={index} className='text-[16px]  w-fit px-4  py-[2px] rounded-full'
+            <span key={index} className='text-[17px] w-fit px-[14px]  py-[2.5px] space-y-2 rounded-full'
             style={{ backgroundColor: skillsBackground,
               color: skillsColor  
             }}
