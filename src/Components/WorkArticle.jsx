92654ba02adfcaa2ff28954a3448d12b27213091
theo-code1 <codingbusiness99@gmail.com>
2025-07-18 16:00:29 +0100

    Add light-black color variable to CSS; update WorkArticle component to include website link and improve layout; modify WorkSection to reflect new website descriptions and links

--- src/Components/WorkArticle.jsx
+++ src/Components/WorkArticle.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import GoArrow from '../assets/Icons/GoArrow'
 
-const WorkArticle = ({ websiteRole, websiteDesc, features }) => {
+const WorkArticle = ({ websiteRole, websiteDesc, features, websiteLink }) => {
   return (
-    <article className='w-full h- border border-white rounded-2xl overflow-hidden font-switzer'>
-        <div className="content-side h-full w-1/2 text-white ">
+    <article className='w-full h- rounded-3xl overflow-hidden font-switzer'>
+        <div className="content-side h-full w-1/2 bg-light-black text-white pl-16 pr-12 py-36  flex flex-col gap-4">
             <h1 className='text-2xl '>{websiteRole}</h1>
-            <p className='text-[16px] text-white/90 '>{websiteDesc}</p>
-            <ul>
+            <p className='text-[16px] text-white/90 mb-4'>{websiteDesc}</p>
+            <ul className='text-lg flex flex-col gap-1 mb-8'>
                 {features.map((feature, index) => (
-                    <li key={index}>{feature}</li>
+                    <li className='list-disc list-inside list-' key={index}>{feature}</li>
                 ))}
             </ul>
+            <a href={websiteLink} className='flex items-center gap-2 text-lg px-4 py-1.5 rounded-lg  w-fit bg-gradient-to-br from-white/20 to-transparent cursor-pointer ' target='_blank' >See Website <GoArrow className='text-2xl' /> </a>
         </div>
 
         <div className="img-side h-full w-1/2  "></div>
--- src/Components/WorkSection.jsx
+++ src/Components/WorkSection.jsx
@@ -7,27 +7,32 @@ const WorkSection = () => {
   const websitesInfo = [
     {
       role: 'website Role',
-      desc: 'website Description',
-      features: ['feature 01', 'feature 02', 'feature 03',]
+      desc: 'lorem lorela lkasdkj askue laksueoiuq laksd oiausro ashjdfieur ajkdoieur aklsjdfur alkcmnxbzco jadhfie jkhasdfu asdasd oiasudadk alksdewr alskjdiu askljdasdjasjd laksueliwue aslkjdlwaue asdhas asejdlauwe asjd asiouaksdh aaskl.',
+      features: ['feature 01', 'feature 02', 'feature 03',],
+      websiteLink: 'https://github.com/theo-code1'
     },
     {
       role: 'website Role',
-      desc: 'website Description',
-      features: ['feature 01', 'feature 02', 'feature 03',]
+      desc: 'lorem lorela lkasdkj askue laksueoiuq laksd oiausro ashjdfieur ajkdoieur aklsjdfur alkcmnxbzco jadhfie jkhasdfu asdasd oiasudadk alksdewr alskjdiu askljdasdjasjd laksueliwue aslkjdlwaue asdhas asejdlauwe asjd asiouaksdh aaskl.',
+      features: ['feature 01', 'feature 02', 'feature 03',],
+      websiteLink: '#'
     },
   ]
 
   return (
-    <section id='work-section' className="h-[200dvh] bg-black max-w-screen overflow-x-hidden relative pt-24 font-switzer px-42 ">
+    <section id='work-section' className="h-[200dvh] max-w-screen flex flex-col gap-16 bg-black overflow-x-hidden relative pt-24 font-switzer px-42 ">
       <div className="headers flex flex-col gap-8">
         <div className="work-label relative flex items-center gap-2 w-fit pl-1.5 pr-8 py-1 rounded-full z-20 shadow-[inset_0_0_15px_2px_rgba(255,255,255,0.1)] shadow-[inset_0_2px_8px_rgba(255,255,255,0.15)  inset-shadow-white inset-shadow-2xs inset-shadow- bg-white/10 backdrop-blur-sm border border-white/20">
           <GlobalIcon className='text-white/90 text-4xl' />
           <h2 className="text-white/90 text-2xl font-[400]">Work</h2>
         </div>
-        <h3 className="text-[16px] text-white/90 font-medium">Developed 10+ sleek, visually stunning and high-performance websites using <br /> different technology like React.js and Tailwind CSS</h3>
+        <h3 className="text-[16px] text-white/90 font-medium">Developed 4+ sleek, visually stunning and high-performance websites using <br /> different technology like React.js and Tailwind CSS</h3>
       </div>
+      <div className="work-articles flex flex-col gap-12 ">
 
-      <WorkArticle websiteRole={websitesInfo[0].role} websiteDesc={websitesInfo[0].desc} features={websitesInfo[0].features} />
+      <WorkArticle websiteRole={websitesInfo[0].role} websiteDesc={websitesInfo[0].desc} features={websitesInfo[0].features} websiteLink={websitesInfo[0].websiteLink} />
+
+      </div>
 
     </section>
   );
