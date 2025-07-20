28be592fd9450d17e454aff14f47ae483edfda12
theo-code1 <codingbusiness99@gmail.com>
2025-07-20 16:23:22 +0100

    Remove OfferCard component and integrate its functionality directly into OfferSection; update TestimonialSection layout for improved presentation and clarity.

--- src/Components/OfferSection.jsx
+++ src/Components/OfferSection.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { motion } from "framer-motion";
-import OfferCard from "./OfferCard";
 import CodeIcon from "../assets/Icons/CodeIcon";
 import WebsiteIcon from "../assets/Icons/WebsiteIcon";
 import DesignIcon from "../assets/Icons/DesignIcon";
@@ -118,3 +117,32 @@ const OfferSection = () => {
 };
 
 export default OfferSection;
+
+
+
+export const OfferCard = ({ IconChild, OfferTitle, OfferDesc, skillsBackground, skillsColor, skills }) => {
+
+
+
+  return (
+    <article className='relative font-switzer w-[450px] h-[ px-8 pt-16 pb-6 rounded-xl bg-white  flex flex-col gap- items-center justify-between border border-black/20 shadow-[0_2px_24px_0_var(--color-shadow-black)] hover:shadow-[0_4px_32px_0_var(--color-shadow-black)] transition-all duration-200 cursor-pointer'>
+        <div className="offer-icon absolute -top-8 left-1/2 -translate-x-1/2 w-20 "
+        >
+            {IconChild}
+        </div>
+        <h1 className='text-[28px] font-semibold text-black mb-8'>{OfferTitle}</h1>
+        <p className='text-lg text-black/75 mb-16'>{OfferDesc}</p>
+        <div className="skills flex flex-wrap self-end gap-2 ">
+          {skills.map((skill, index) => (
+
+            <span key={index} className='text-[17px] w-fit px-[14px]  py-[2.5px] space-y-2 rounded-full'
+            style={{ backgroundColor: skillsBackground,
+              color: skillsColor  
+            }}
+            >{skill}</span>
+          ))}
+        </div>
+    </article>
+  )
+}
+
--- src/Components/TestimonialSection.jsx
+++ src/Components/TestimonialSection.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const TestimonialSection = () => {
   return (
-    <section className='h-screen bg-white flex items-center justify-center '>
-        <h1 className='text-4xl text-black font medium font-switzer text-center'>Testimonial</h1>
+    <section className='h-screen bg-white flex flex-col items-center gap-16 pt-24 font-switzer '>
+      <div className="heading flex flex-col text-center gap-2">
+          <h1 className='text-[2rem] text-black font-semibold text-center'>What people say matters</h1>
+          <p className='text-center text-sm text-black/50 '>Insights From People Who’ve Seen My Work in Action</p>
+      </div>
     </section>
   )
 }
