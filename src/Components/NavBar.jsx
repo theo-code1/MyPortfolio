833e59b825f0aacc37c3b2699411d78e075ad7db
theo-code1 <codingbusiness99@gmail.com>
2025-07-20 03:34:32 +0100

    Refactor NavBar styling for improved layout; update WorkArticle to conditionally render website link based on StatusLabel; enhance AllWorkSection project descriptions for clarity and detail, improving overall presentation.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -31,7 +31,7 @@ import { Link } from 'react-router';
             <h1 className='text-2xl font-switzer font-medium '>TheoKode</h1>
         </div>
 
-        <ul className={`px-8 py-3 rounded-full shadow-[0_3px_10px_0_var(--color-shadow-black)] border border-black/10 flex items-center gap-16 text-[16px] font-switzer font-medium ${isScrolled ? 'mx-auto bg-white/50 backdrop-blur-sm' : 'bg-white'}`}>
+        <ul className={`px-8 py-2 rounded-full shadow-[0_3px_10px_0_var(--color-shadow-black)] border border-black/10 flex items-center gap-16 text-[16px] font-switzer font-medium ${isScrolled ? 'mx-auto bg-white/50 backdrop-blur-sm' : 'bg-white'}`}>
             <li onClick={() => scrollToSection(overviewRef)}  className='cursor-pointer text-black hover:text-dark-blue/90 px-2 py-1 transition-all duration-200'>Overview</li>
             <li onClick={() => scrollToSection(aboutRef)}  className='cursor-pointer text-black hover:text-dark-blue/90 px-2 py-1 transition-all duration-200'>About</li>
             <li onClick={() => scrollToSection(workRef)}  className='cursor-pointer text-black hover:text-dark-blue/90 px-2 py-1 transition-all duration-200'>Work</li>
